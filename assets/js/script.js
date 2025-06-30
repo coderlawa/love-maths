0d008cae49f2db7b8bafa4f935fdd8cde01c1d95
coderlawa <5d12tpff@students.codeinstitute.net>
2025-06-30 23:56:05 +0100

    tidied up code for UX

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,13 +15,23 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  runGame("addition");
+  document
+    .getElementById("answer-box")
+    .addEventListener("keydown", function (event) {
+      if (event.key === "Enter") {
+        checkAnswer();
+      }
 
+      runGame("addition");
+    });
   /**
    * The main game "loop", called when the script is first loaded
    * and after the user's answer has been processed.
    */
   function runGame(gameType) {
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     // create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -31,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function () {
     } else if (gameType === "multiply") {
       displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
-        displaySubtractQuestion(num1, num2);
+      displaySubtractQuestion(num1, num2);
     } else {
       alert(`Unknown game type: ${gameType}`);
       throw `Unknown game type: ${gameType}. Aborting!`;
@@ -106,11 +116,11 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function displaySubtractQuestion(operand1, operand2) {
-    
-    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operand1").textContent =
+      operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent =
+      operand1 > operand2 ? operand2 : operand1;
     document.getElementById("operator").textContent = "-";
-    
   }
 
   function displayMultiplyQuestion(operand1, operand2) {
